5ba82f667d2837ce386a94ec8e885dcd40d219c8
Alexey Volkov <avolkov@google.com>
2024-05-25 01:41:52 -0700

    chore: Fixed all linting issues and enabled linting in vertexai
    PiperOrigin-RevId: 637165523

--- vertexai/preview/reasoning_engines/templates/langchain.py
+++ vertexai/preview/reasoning_engines/templates/langchain.py
@@ -13,17 +13,14 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-import json
 from typing import (
     TYPE_CHECKING,
     Any,
     Callable,
     Dict,
-    List,
     Mapping,
     Optional,
     Sequence,
-    Tuple,
     Union,
 )
 
